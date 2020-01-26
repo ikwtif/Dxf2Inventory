3bd3f778c5391258ed62d73120f382e784ab8ee0
ikwtif <ikwtif@users.noreply.github.com>
2020-01-26 21:29:10 +0100

    add check to ignore when vector is None

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ def parse_detail_header(query_mtext):
     return balken_header
 
 
-
 def parse_detail_info(query_text):
     """
     :param query_text: ezdxf query for text
@@ -86,8 +85,6 @@ query_mtext_tekstlayer = msp.query('MTEXT [layer=="tekst"]')
 balken_header = parse_detail_header(query_mtext_tekstlayer)
 logging.info(f'got detail headers for {balken_header}')
 
-
-
 logging.info(f'get all text from dxf in layer tekst')
 query_text_tekstlayer = msp.query('TEXT [layer=="tekst"]')
 balken_detail_info = parse_detail_info(query_text_tekstlayer)
@@ -96,6 +93,7 @@ logging.info(f'got detail info for {balken_detail_info}')
 
 #TODO get all length values from details
 
+
 def parse_detail_lengths(query_text):
     """
     :param query_text: dxf query of text containing length information of a detail
@@ -103,11 +101,16 @@ def parse_detail_lengths(query_text):
     """
     #TODO check for closest here to evaluate the different possible rebar bending techniques?
     # or put info inside dxf itself? -> more prone to error? double checks? do both?
+    # sometimes no vector but is None??
     logging.info(f'starting parsing length info for {query_text}')
     balken_detail_lengths = list()
     for part in query_text:
-        balken_detail_lengths.append([part.dxf.text, part.dxf.align_point])
+        if part.dxf.align_point is None:
+            pass
+        else:
+            balken_detail_lengths.append([part.dxf.text, part.dxf.align_point])
     return balken_detail_lengths
+
 
 logging.info(f'get all length text from dxf in layer 0')
 query_text_0layer = msp.query('TEXT [layer=="0"]')
@@ -117,7 +120,6 @@ logging.info(f'get length info for details {balken_detail_lengths}')
 #TODO analyze closest vectors for header and wapening parts //ADD CHECKS FOR ACCURACY BASED ON?
 # for wapening en length!!!! -> both in one function?
 # start from header and find closest wapening parts
-
 def vector_conditions(self, other, condition):
     """
     determines correct side of the new coordinate position compared to base position of old coordinate
@@ -192,8 +194,6 @@ for balk in balken_header.keys():
                 balken_header[balk][number_wapening] = item
 
 
-
-
 #TODO analyze closest vectors for wapening lengths //ADD CHECKS FOR ACCURACY BASED ON?
 # relate to header first, then to wapening number -> different tolerance -- just add to tolerance for wapening for x-axis?!
 logging.info(f'start comparing position vectors for header/info and lengths')
@@ -205,9 +205,6 @@ print('lengts', balken_detail_lengths)
 
 for balk in balken_header.keys():
     for number in balken_header[balk].keys():
-        print('-'*50)
-        print('number', number, balk)
-
         if number != 'position_vector':
             position_wapening = balken_header[balk][number]['position_vector']
             position_header = balken_header[balk]['position_vector']
